6f6b28a4fde1e0f538463b4b777e237b7666bc5a
Victor Graciano <victor.graciano@usp.br>
2019-06-21 20:20:10 +0000

    Update Volunteers Page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module WebsitePets
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.precompile += %w( pets.js )
+    config.assets.paths << Rails.root.join("app", "assets", "images")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
